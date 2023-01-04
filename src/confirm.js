5e0a083112569e73e0c9c1593b565e76789fd432
Felicia Chen <chenzy@Chens-MacBook-Pro.local>
2023-01-04 16:46:31 -0500

    Initial

---
+++ src/confirm.js
@@ -0,0 +1,8 @@
+// import { Auth, Amplify } from 'aws-amplify'
+// import config from './aws-exports'
+
+// Amplify.configure(config)
+
+// document.querySelector('#confirm-form').addEventListener('submit', async e => {
+//   e.preventDefault()
+// })
---
+++ src/script.js
@@ -0,0 +1,16 @@
+// let currentUser = null
+
+// const toggleNavBar = () => {
+//   if (currentUser) {
+//     document.querySelector('.logged-in').classList.add('hidden')
+//     document.querySelector('.logged-in').classList.add('hidden')
+//     document.querySelector('#sign-out').classList.remove('hidden')
+//     document.querySelector('#create-post').classList.remove('hidden')
+//   } else {
+//     document.querySelector('.logged-in').classList.remove('hidden')
+//     document.querySelector('.logged-in').classList.remove('hidden')
+//     document.querySelector('#sign-out').classList.add('hidden')
+//     document.querySelector('#create-post').classList.add('hidden')
+//   }
+// }
+s
