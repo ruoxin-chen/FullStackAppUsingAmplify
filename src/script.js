f4bbf9b5401e76b1bc2a002159a5128487aa1271
Felicia Chen <chenzy@Chens-MacBook-Pro.local>
2023-01-09 16:06:49 -0500

    backup files

--- src/script.js
+++ src/script.js
@@ -5,58 +5,59 @@ import { Post, Author } from "./models";
 Amplify.configure(awsconfig);
 
 document.getElementById('create-post').addEventListener('submit', async e => {
-    e.preventDefault()
-  
-    try {
-      const file = document.getElementById('img').files[0]
-      await Storage.put(file.name, file) // Store it in DynamoDB, S3
-      
-      // Write data to the DataStore by passing an instance of a data model
-      const newPost = await DataStore.save(new Post({
-        description: document.getElementById('description').value, // input text in index.html
-        image: file.name
-      })).then(() => {
-        alert("Post succeeded!") // Only when the previous code does not raise an error
-        window.location.reload() // Refresh the page
-      })
-      console.log(newPost)
-    } catch (err) {
-      console.log(err)
-    }
-  })
+  e.preventDefault()
+
+  try {
+    const file = document.getElementById('img').files[0]
+    await Storage.put(file.name, file) // Store it in DynamoDB, S3
+    
+    // Write data to the DataStore by passing an instance of a data model
+    const newPost = await DataStore.save(new Post({
+      description: document.getElementById('description').value, // input text in index.html
+      image: file.name
+    })).then(() => {
+      alert("Post succeeded!") // Only when the previous code does not raise an error
+      window.location.reload() // Refresh the page
+    })
+    console.log(newPost)
+  } catch (err) {
+    console.log(err)
+  }
+})
 
 const pullData = async () => {
-    try {
-      const posts = await DataStore.query(Post) // Query for all items
-      let postsWithPic = [] // initialize an array
-      for (const post of posts) {
-        try {
-          let img = await Storage.get(post.image) // Get image from S3
+  try {
+    const posts = await DataStore.query(Post) // Query for all items
+    let postsWithPic = [] // initialize an array
+    for (const post of posts) {
+      try {
+        let img = await Storage.get(post.image) // Get image from S3
         postsWithPic.push({
           description: post.description,
           pic:img
         })
-      } catch (err) {
-        postsWithPic.push({
-        description: post.description,
-        pic: post.link
-        })
-      } 
-      }
-      const postsDiv = document.querySelector('.posts')
-      postsWithPic.map(post => {
-        const postDiv = document.createElement('div')
-        postDiv.classList.add('post')
-        const img = document.createElement('img')
-        const p = document.createElement('p')
-        p.innerText = post.description
-        img.setAttribute('src', post.pic)
-        postDiv.appendChild(img)
-        postDiv.appendChild(p)
-        postsDiv.appendChild(postDiv)
+    } catch (err) {
+      postsWithPic.push({
+      description: post.description,
+      pic: post.link 
       })
-  } catch (err) {
-      console.log(err)
+    } 
+    }
+    const postsDiv = document.querySelector('.posts')
+    // for each post, create a div and add img and p under div
+    postsWithPic.map(post => {
+      const postDiv = document.createElement('div')
+      postDiv.classList.add('post')
+      const img = document.createElement('img')
+      const p = document.createElement('p')
+      p.innerText = post.description
+      img.setAttribute('src', post.pic)
+      postDiv.appendChild(img)
+      postDiv.appendChild(p)
+      postsDiv.appendChild(postDiv)
+    })
+} catch (err) {
+    console.log(err)
   }
 }
   
@@ -82,22 +83,37 @@ const toggleNavBar = () => {
 
 // Call toggleNavBar when users have signed in 
 const getCurrentUser = async () => {
-    try {
-      currentUser = await Auth.currentAuthenticatedUser()
-    } catch (err) {
-      console.log('error getting user', err)
-      currentUser = null
-    }
-    toggleNavBar()
+  try {
+    currentUser = await Auth.currentAuthenticatedUser()
+  } catch (err) {
+    console.log('error getting user', err)
+    currentUser = null
   }
+  toggleNavBar()
+}
 getCurrentUser()
 
 // Sign out handler 
 document.getElementById('sign-out').addEventListener('click', async e => {
-    try {
-        await Auth.signOut({ global: true });
-        window.location.href = '/'
-    } catch (error) {
-        console.log('error signing out: ', error);
-    }
+  try {
+      await Auth.signOut({ global: true });
+      window.location.href = '/'
+  } catch (error) {
+      console.log('error signing out: ', error);
+  }
 }) 
+
+const buttonArray = []
+const signIn = document.getElementById("sign-in")
+const signUp = document.getElementById("sign-up")
+const signOut = document.getElementById("sign-out")
+const aboutUs = document.getElementById("about-us")
+buttonArray.push(signIn, signUp, signOut, aboutUs)
+for (const button of buttonArray) {
+  button.addEventListener("mouseenter", () => {
+    button.style.color = "#ecc440";
+    })
+  button.addEventListener("mouseleave", () => {
+    button.style.color = "whitesmoke";
+  })
+}
